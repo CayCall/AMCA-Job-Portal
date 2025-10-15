55b23aae9fb08471b9ea3e0194d4f9836b248d58
Caleb Cameron Calvin <calvincaleb09@gmail.com>
2025-10-16 00:06:05 +0200

    some changes

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ import express from 'express'
 import 'dotenv/config'
 import connectDB from './config/db.js'
 import './config/instrument.js'
-import "../src/config/instrument.js";
+import "./config/instrument.js";
 import * as Sentry from "@sentry/node";
 import { clerkwebHook } from './routes/webhooks.js'
 
@@ -16,12 +16,13 @@ const app = express()
 //MONGO DB CONN
 connectDB()
 
-//Middleware - transition layer (req(), res(), next())- sits between the request and the response
+//Middlewares - transition layer (req(), res(), next())- sits between the request and the response
 //Runs after a request comes in but before the server sends a response.
 app.use(cors())
+
+app.post('/webhooks', clerkwebHook)
+
 app.use(express.json())
-
-
 //Route
 app.get('/', (req, res) => res.send("Api Working."))
 app.get('/debug-sentry', function mainHandler(req, res) {
@@ -30,14 +31,11 @@ app.get('/debug-sentry', function mainHandler(req, res) {
 app.get('/db-disconnected', function mainHandler(req, res) {
     throw new Error("Db disconnected!")
 });
-app.post('/webhooks', clerkwebHook)
-//Port
-const PORT = process.env.PORT || 5000
+
 
 Sentry.setupExpressErrorHandler(app);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
-
 //idk
+
+
+export default app;
