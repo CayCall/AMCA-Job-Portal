f324a481e7e777968f381d6ab62416329497e2ff
Caleb Cameron Calvin <calvincaleb09@gmail.com>
2025-10-17 01:48:54 +0200

    dev notes
    - keeping my process in check

--- server/src/server.js
+++ server/src/server.js
@@ -7,24 +7,39 @@ import "./config/instrument.js";
 import * as Sentry from "@sentry/node";
 import { clerkwebHook } from './routes/webhooks.js'
 
+/*
+    Server Architecture :
+    1. Front-End(Client) - to send requests (http intents(POST,GET)) to the server, e.g user signing up or conmpleting form
+    2. Back-End(Server) - talk to db, receive requests, return data
+    3. webhooks(third party)- like clerk telling the server to create a user for example.
+
+*/
+
 //this will initilise an express app 
 //will allows the app to process http requests, manage certain routes and incorporate middleware("transition layer" - DB, sign ins)
-
 const app = express()
 
 
-//MONGO DB CONN
+//MONGO DB function to connnect database on mongodb server
 connectDB()
 
-//Middlewares - transition layer (req(), res(), next())- sits between the request and the response
+
+//Middleware - transition layer (req(), res(), next())- sits between the http request and the response
 //Runs after a request comes in but before the server sends a response.
 app.use(cors())
 
+
+//browser will send http request to webhook url, and run clerk webhook function 
 app.post('/webhooks', clerkwebHook)
 
 app.use(express.json())
-//Route
-app.get('/', (req, res) => res.send("Api Working."))
+
+/* Routing is most important for the GET and POST intentions aka http requests, with the server.
+       we will GET (.get()) from a route to fetch data
+       we will POST (.post()) to a route to send data */
+
+
+app.get('/', (req, res) => res.send("Server is up and running.")) // getting the home('/') route to invoke a response that tells us the Server is working
 app.get('/debug-sentry', function mainHandler(req, res) {
     throw new Error("")
 });
@@ -34,8 +49,5 @@ app.get('/db-disconnected', function mainHandler(req, res) {
 
 
 Sentry.setupExpressErrorHandler(app);
-
-//idk
-
 
 export default app;
